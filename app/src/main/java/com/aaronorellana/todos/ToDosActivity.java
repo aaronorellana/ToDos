29480f52c0c7d11a5c8f5069861d8c5b93bc63e6
Aaron Orellana <aaronorellana1@gmail.com>
2018-10-10 17:29:46 -0400

    Add Files to Init

---
+++ app/src/main/java/com/aaronorellana/todos/ToDosActivity.java
@@ -0,0 +1,13 @@
+package com.aaronorellana.todos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToDosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_dos);
+    }
+}
